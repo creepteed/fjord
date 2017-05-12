a61035657f68677daa63385eade28cef8322b414
creepteedLTD <creepteed@gmail.com>
2017-05-12 16:07:57 +0300

    'r2r'

--- app/js/common.js
+++ app/js/common.js
@@ -31,4 +31,25 @@ $(document).ready(function () {
         spaceBetween: 30,
         freeMode: true
     });
+
+    $('.page').click(function() {
+        $(this).removeClass('no-anim').toggleClass('flipped');
+        $('.page > div').click(function(e) {
+            e.stopPropagation();
+        });
+        reorder()
+    });
+    function reorder(){
+        $(".book").each(function(){
+            var pages=$(this).find(".page")
+            var pages_flipped=$(this).find(".flipped")
+            pages.each(function(i){
+                $(this).css("z-index",pages.length-i)
+            })
+            pages_flipped.each(function(i){
+                $(this).css("z-index",i+1)
+            })
+        });
+    }
+    reorder()
 });
